50b6f79265ac0690271f0a410bee23100e91a4ee
John Bassey <jsbazman@gmail.com>
2022-10-01 15:46:09 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -32,7 +32,7 @@ def process_song_data(spark, input_data, output_data):
     songs_table = df.select(["song_id", "title", "artist_id", "year", "duration"])
 
     #write songs table to parquet files partitioned by year
-    songs_table.write.parquet(os.path.join*(output_data,'song'), partitionBy = ['year', 'artist_id'])
+    songs_table.write.parquet(os.path.join(output_data,'song'), partitionBy = ['year', 'artist_id'])
 
     #extract columns to create artits table
     columns = ['artist_name', 'artist_location','artist_latitude','artist_longitude']
@@ -75,7 +75,7 @@ def process_log_data(spark, input_data, output_data):
     time_table = time_table.withColumn('hour', F.hour('start_time'))
     time_table  = time_table.withColumn('day', F.dayofmonth('start_time'))
     time_table = time_table.withColumn('week', F.weekofyear('start_time'))
-    time_table = time_table.withColumn('month', F.monht('start_time'))
+    time_table = time_table.withColumn('month', F.month('start_time'))
     time_table = time_table.withColumn('year', F.year('start_time'))
     time_table = time_table.withcolumn('weekday', F.dayofweek('start_time'))
 
